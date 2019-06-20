72993e2fe8ddb9d72c4dbf1548f0d5b8301d3480
Elizabeth <syaora@csu.fullerton.edu>
2019-06-20 06:57:37 -0700

    Updated styles

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Login from './Login';
 import Dashboard from './Dashboard';
+import { Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
@@ -10,14 +12,22 @@ function App() {
       <div>
         <main>
           <article>
-            <nav>
-              <Link to='/login'>Login</Link>
-              <br />
-              <Link to='/register'>Register</Link>
-              <br />
-              <Link to='/dashboard'>Dashboard</Link>
-              <br />
-            </nav>
+            <Navbar bg="primary" variant="dark">
+              <Navbar.Brand href="">Brand</Navbar.Brand>
+              <Navbar.Collapse>
+                <Nav className="ml-auto">
+                  <LinkContainer to='/login'>
+                    <Nav.Link>Login</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/register'>
+                    <Nav.Link>Register</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/dashboard'>
+                    <Nav.Link>Dashboard</Nav.Link>
+                  </LinkContainer>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
             <Route exact path='/login' component={Login} />
             <Route exact path='/dashboard' component={Dashboard} />
           </article>
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import Table from './Table';
+import { Container, Row, Button } from 'react-bootstrap';
+import AddBuyForm from './AddBuyForm';
+
 export default class Dashboard extends Component {
+  constructor(...args) {
+		super(...args);
+
+		this.state = { modalShow: false };
+  }
+  
   render() {
+    let modalClose = () => this.setState({ modalShow: false });
+
     return (
-      <div>
-        <Table />
-      </div>
+      <Container className="mt-3">
+        <Row>
+          <Button
+          variant="primary"
+          onClick={() => this.setState({ modalShow: true })}
+          >
+            Add Transaction
+          </Button>
+        </Row>
+
+        <AddBuyForm
+        show={this.state.modalShow}
+        onHide={modalClose}
+        />
+
+        <br/>
+        <Row>
+          <Table />
+        </Row>
+      </Container>
     );
   }
 }
