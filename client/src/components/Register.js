1a71f1c621a181792fb16ba08ed9d5c1a5c69bf1
Elizabeth <syaora@csu.fullerton.edu>
2019-07-01 11:25:27 -0700

    Updated validation and alerts

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
-
+import { Form, Button, Alert } from 'react-bootstrap/';
 import { AppContextConsumer } from '../contexts/AppContext';
 
+
 export default class Register extends Component {
-	
   state = {
     firstName: 'TestFirst',
     lastName: 'TestLast',
@@ -27,23 +27,27 @@ export default class Register extends Component {
 		if (form.checkValidity() === false) {
 			event.preventDefault();
 			event.stopPropagation();
-		} else {
-			this.setState({validated: true});
+		}
+		this.setState({validated: true});
+		event.preventDefault();
+		
+		try {
+			const response = await axios.post('/api/users', this.state);
+			console.log(response);
+			localStorage.setItem('token', response.data.token);
 
-			// try {
-			// 	const response = await axios.post('/api/users', this.state);
-			// 	console.log(response);
-			// 	localStorage.setItem('token', response.data.token);
-
-			// 	//need to redirect user to /dashboard...
-			// } catch (error) {
-			// 	this.setState({ error: true, errorMessage: 'Unable to log in.' });
-			// }
+			//need to redirect user to /dashboard...
+		} catch (error) {
+			this.setState({ error: true, errorMessage: 'Unable to register.' });
 		}
   };
 
   render() {
-		const { validated } = this.state.validated;
+		//Used to handle form validation
+		const { validated } = this.state;
+
+		//Used to handle error message
+		const handleDismiss = () => this.setState({ error: false });
 
     return (
       <AppContextConsumer>
@@ -53,45 +57,69 @@ export default class Register extends Component {
           } else {
             return (
               <div>
-                <form
-                  onSubmit={e => {
-                    e.preventDefault();
-                    console.log('event:', e);
-                    register(this.state);
-                  }}
+								<Alert variant="danger" show={this.state.error} onClose={handleDismiss} dismissible>
+									<p className="ma-0">{this.state.errorMessage}</p>
+								</Alert>
+
+                <Form
+								 noValidate
+								 validated={validated}
+                 onSubmit={e => {
+									console.log('event:', e);
+									this.handleSubmit(e);
+									//register(this.state);
+                 }}
                 >
-                  <label>First Name: </label>
-                  <input
-                    name='firstName'
-                    value={this.state.firstName}
-                    onChange={this.handleChange}
-                  />
-                  <label>Last Name: </label>
-                  <input
-                    name='lastName'
-                    value={this.state.lastName}
-                    onChange={this.handleChange}
-                  />
-                  <label>Email: </label>
-                  <input
-                    name='email'
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                  />
-                  <label>Password: </label>
-                  <input
-                    type='password'
-                    name='password'
-                    placeholder='Password'
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                  />
-                  <br />
-                  {this.state.error === true && (
-                    <p>{this.state.errorMessage}</p>
-                  )}
-                  <input type='submit' value='Submit' />
-                </form>
+									<Form.Group>
+										<Form.Label>First Name:</Form.Label>
+										<Form.Control
+										 type="text"
+										 placeholder="John"
+										 value={this.state.firstName}
+										 onChange={this.handleChange}
+										 required
+										/>
+										<Form.Control.Feedback type='invalid'>Please enter first name.</Form.Control.Feedback>
+									</Form.Group>
+                  
+									<Form.Group>
+										<Form.Label>Last Name:</Form.Label>
+										<Form.Control
+										 type='text'
+										 placeholder='Doe'
+										 value={this.state.lastName}
+										 onChange={this.handleChange}
+										 required
+										/>
+										<Form.Control.Feedback type='invalid'>Please enter a last name.</Form.Control.Feedback>
+									</Form.Group>
+                  
+									<Form.Group>
+										<Form.Label>Email:</Form.Label>
+										<Form.Control
+										 type='email'
+										 value={this.state.email}
+										 onChange={this.handleChange}
+										 required
+										/>
+										<Form.Control.Feedback type='invalid'>Please enter an email.</Form.Control.Feedback>
+									</Form.Group>
+                  
+									<Form.Group>
+										<Form.Label>Password:</Form.Label>
+										<Form.Control
+										 type='password'
+										 value={this.state.password}
+										 onChange={this.handleChange}
+										 pattern='.{6,}'
+										 required
+										/>
+										<Form.Control.Feedback type='invalid'>Please enter a password more than 6 characters.</Form.Control.Feedback>
+									</Form.Group>
+									<Button variant="primary" type="submit">
+										Submit
+									</Button>
+                </Form>
               </div>
             );
           }
--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import Table from 'react-bootstrap/Table';
+import axios from 'axios';
 
 import { AppContextConsumer } from '../contexts/AppContext';
+import { resolve } from 'path';
 class DisplayTable extends Component {
-  constructor(...args) {
-    super(...args);
+	state = { 
+		modalShow: false, 
+		stocks: []
+	}
+	
+	getCurrentPrice = async(ticker) => {
+		const url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + ticker + '&apikey=1L6GZW4SFVR1BCVB';
+		//Update current price
+		// let res = await axios.get(url)
+		// let price = res.data["Global Quote"]["05. price"];
+		// console.log(price);
+			// if (price) {
+			// 	console.log('passed')
+		// console.log(typeof(price));
+		// return price;
+			// 	console.log('test');
+			// 	console.log(this.state.currentPrice);
+			// }
+			// resolve(response);
+		// })
+		//Returns value of current price
+		//return this.state.currentPrice
+	}
 
-    this.state = { modalShow: false, stocks: [] };
-  }
+	getReturn(currentPrice, buyPrice) {
+		if (!currentPrice || !buyPrice) {
+			return 'N/A';
+		}
+		let returnPer = currentPrice / buyPrice;
+		returnPer = (returnPer - 1) * 100;
+		return returnPer;
+	}
+
   render() {
     const headings = [
       'Ticker',
@@ -18,7 +48,7 @@ class DisplayTable extends Component {
       'Buy Date',
       'Shares',
       'Buy Price'
-    ];
+		];
 
     const theadMarkup = (
       <thead>
@@ -28,7 +58,7 @@ class DisplayTable extends Component {
           })}
         </tr>
       </thead>
-    );
+		);
 
     return (
       <AppContextConsumer>
@@ -39,9 +69,11 @@ class DisplayTable extends Component {
               {stocks.map(stock => (
                 <tr key={stock.ticker}>
                   <td>{stock.ticker}</td>
+									<td>
+										{this.getCurrentPrice(stock.ticker)}
+									</td>
                   <td>--</td>
-                  <td>--</td>
-                  <td>--</td>
+                  <td>{this.getReturn(this.state.currentPrice, stock.price)}</td>
                   <td>--</td>
                   <td>{stock.date}</td>
                   <td>{stock.quantity}</td>
