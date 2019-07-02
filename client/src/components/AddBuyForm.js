4671b8ac072e41dea0195cbcdf3a6dc3e7cbc55b
Elizabeth <syaora@csu.fullerton.edu>
2019-07-01 21:30:51 -0700

    Adjusted spacing and added redirects

--- client/src/components/AddBuyForm.js
+++ client/src/components/AddBuyForm.js
@@ -5,76 +5,76 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
 
 class AddBuyForm extends Component {
-	constructor(...args) {
-		super(...args);
+  constructor(...args) {
+    super(...args);
 
-		this.state = { validated: false };
-	}
+    this.state = { validated: false };
+  }
 
-	handleSubmit(event) {
-		const form = event.currentTarget;
-		if (form.checkValidity() === false) {
-			event.preventDefault();
-			event.stopPropagation();
-		}
-		this.setState({ validated: true });
-	}
+  handleSubmit(event) {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    this.setState({ validated: true });
+  }
 
   render() {
-		const { validated } = this.state;
+    const { validated } = this.state;
 
-		return (
-			<Modal
-			 {...this.props}
-			 centered
-			 >
-				<Modal.Header closeButton>
-					<Modal.Title id="contained-modal-title-vcenter">
-						Add Buy Transaction
-					</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<Form
-					 noValidate
-					 validated={validated}
-					 onSubmit={e => this.handleSubmit(e)}
-					>
-						<Form.Group controlId="formTicker">
-							<Form.Label>Ticker</Form.Label>
-							<Form.Control 
-							 type="text" 
-							 placeholder="Enter Ticker"
-							 required 
-							/>
-							<Form.Control.Feedback type='invalid'>Please enter a Ticker.</Form.Control.Feedback>
-						</Form.Group>
-						
-						<Form.Group controlId="formDate">
-							<Form.Label>Date</Form.Label>
-							<Form.Control required type="date" />
-							<Form.Control.Feedback type='invalid'>Please select a date.</Form.Control.Feedback>
-						</Form.Group>
-						
-						<Form.Group controlId="formQuantity">
-							<Form.Label>Quantity</Form.Label>
-							<Form.Control required type="number" min='0' />
-							<Form.Control.Feedback type='invalid'>Please enter a quantity.</Form.Control.Feedback>
-						</Form.Group>
-						
-						<Form.Group controlId="formPricePerShare">
-							<Form.Label>Price Per Share</Form.Label>
-							<Form.Control required type="text" />
-							<Form.Control.Feedback type='invalid'>Please enter a price per share.</Form.Control.Feedback>
-						</Form.Group>
-						
-						<Button variant="primary" type="submit">
-							Submit
-						</Button>
-					</Form>
-				</Modal.Body>
-			</Modal>
-		)
-	}
+    return (
+      <Modal
+       {...this.props}
+       centered
+       >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Add Buy Transaction
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form
+           noValidate
+           validated={validated}
+           onSubmit={e => this.handleSubmit(e)}
+          >
+            <Form.Group controlId="formTicker">
+              <Form.Label>Ticker</Form.Label>
+              <Form.Control 
+               type="text" 
+               placeholder="Enter Ticker"
+               required 
+              />
+              <Form.Control.Feedback type='invalid'>Please enter a Ticker.</Form.Control.Feedback>
+            </Form.Group>
+            
+            <Form.Group controlId="formDate">
+              <Form.Label>Date</Form.Label>
+              <Form.Control required type="date" />
+              <Form.Control.Feedback type='invalid'>Please select a date.</Form.Control.Feedback>
+            </Form.Group>
+            
+            <Form.Group controlId="formQuantity">
+              <Form.Label>Quantity</Form.Label>
+              <Form.Control required type="number" min='0' />
+              <Form.Control.Feedback type='invalid'>Please enter a quantity.</Form.Control.Feedback>
+            </Form.Group>
+            
+            <Form.Group controlId="formPricePerShare">
+              <Form.Label>Price Per Share</Form.Label>
+              <Form.Control required type="text" />
+              <Form.Control.Feedback type='invalid'>Please enter a price per share.</Form.Control.Feedback>
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    )
+  }
 }
 
 export default AddBuyForm
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -12,9 +12,9 @@ export default class Register extends Component {
     email: 'test@mail.com',
     password: '123456',
     error: false,
-		errorMessage: '',
-		validated: false
-	};
+    errorMessage: '',
+    validated: false
+  };
 
   handleChange = async event => {
     await this.setState({
@@ -24,30 +24,29 @@ export default class Register extends Component {
 
   handleSubmit = async event => {
 		const form = event.currentTarget;
-		if (form.checkValidity() === false) {
-			event.preventDefault();
-			event.stopPropagation();
-		}
-		this.setState({validated: true});
 		event.preventDefault();
-		
-		try {
-			const response = await axios.post('/api/users', this.state);
-			console.log(response);
-			localStorage.setItem('token', response.data.token);
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    }
+    this.setState({validated: true});
+    try {
+      const response = await axios.post('/api/users', this.state);
+      console.log(response);
+      localStorage.setItem('token', response.data.token);
 
-			//need to redirect user to /dashboard...
-		} catch (error) {
-			this.setState({ error: true, errorMessage: 'Unable to register.' });
-		}
+			//Redirect user to /dashboard...
+			this.props.history.push('/dashboard');
+    } catch (error) {
+      this.setState({ error: true, errorMessage: 'Unable to register.' });
+    }
   };
 
   render() {
-		//Used to handle form validation
-		const { validated } = this.state;
+    //Used to handle form validation
+    const { validated } = this.state;
 
-		//Used to handle error message
-		const handleDismiss = () => this.setState({ error: false });
+    //Used to handle error message
+    const handleDismiss = () => this.setState({ error: false });
 
     return (
       <AppContextConsumer>
@@ -57,68 +56,68 @@ export default class Register extends Component {
           } else {
             return (
               <div>
-								<Alert variant="danger" show={this.state.error} onClose={handleDismiss} dismissible>
-									<p className="ma-0">{this.state.errorMessage}</p>
-								</Alert>
+                <Alert variant="danger" show={this.state.error} onClose={handleDismiss} dismissible>
+                  <p className="ma-0">{this.state.errorMessage}</p>
+                </Alert>
 
                 <Form
-								 noValidate
-								 validated={validated}
+                 noValidate
+                 validated={validated}
                  onSubmit={e => {
-									console.log('event:', e);
-									this.handleSubmit(e);
-									//register(this.state);
+                  console.log('event:', e);
+                  this.handleSubmit(e);
+                  //register(this.state);
                  }}
                 >
-									<Form.Group>
-										<Form.Label>First Name:</Form.Label>
-										<Form.Control
-										 type="text"
-										 placeholder="John"
-										 value={this.state.firstName}
-										 onChange={this.handleChange}
-										 required
-										/>
-										<Form.Control.Feedback type='invalid'>Please enter first name.</Form.Control.Feedback>
-									</Form.Group>
+                  <Form.Group>
+                    <Form.Label>First Name:</Form.Label>
+                    <Form.Control
+                     type="text"
+                     placeholder="John"
+                     value={this.state.firstName}
+                     onChange={this.handleChange}
+                     required
+                    />
+                    <Form.Control.Feedback type='invalid'>Please enter first name.</Form.Control.Feedback>
+                  </Form.Group>
                   
-									<Form.Group>
-										<Form.Label>Last Name:</Form.Label>
-										<Form.Control
-										 type='text'
-										 placeholder='Doe'
-										 value={this.state.lastName}
-										 onChange={this.handleChange}
-										 required
-										/>
-										<Form.Control.Feedback type='invalid'>Please enter a last name.</Form.Control.Feedback>
-									</Form.Group>
+                  <Form.Group>
+                    <Form.Label>Last Name:</Form.Label>
+                    <Form.Control
+                     type='text'
+                     placeholder='Doe'
+                     value={this.state.lastName}
+                     onChange={this.handleChange}
+                     required
+                    />
+                    <Form.Control.Feedback type='invalid'>Please enter a last name.</Form.Control.Feedback>
+                  </Form.Group>
                   
-									<Form.Group>
-										<Form.Label>Email:</Form.Label>
-										<Form.Control
-										 type='email'
-										 value={this.state.email}
-										 onChange={this.handleChange}
-										 required
-										/>
-										<Form.Control.Feedback type='invalid'>Please enter an email.</Form.Control.Feedback>
-									</Form.Group>
+                  <Form.Group>
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control
+                     type='email'
+                     value={this.state.email}
+                     onChange={this.handleChange}
+                     required
+                    />
+                    <Form.Control.Feedback type='invalid'>Please enter an email.</Form.Control.Feedback>
+                  </Form.Group>
                   
-									<Form.Group>
-										<Form.Label>Password:</Form.Label>
-										<Form.Control
-										 type='password'
-										 value={this.state.password}
-										 onChange={this.handleChange}
-										 pattern='.{6,}'
-										 required
-										/>
-										<Form.Control.Feedback type='invalid'>Please enter a password more than 6 characters.</Form.Control.Feedback>
-									</Form.Group>
-									<Button variant="primary" type="submit">
-										Submit
-									</Button>
+                  <Form.Group>
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                     type='password'
+                     value={this.state.password}
+                     onChange={this.handleChange}
+                     pattern='.{6,}'
+                     required
+                    />
+                    <Form.Control.Feedback type='invalid'>Please enter a password more than 6 characters.</Form.Control.Feedback>
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
                 </Form>
               </div>
             );
